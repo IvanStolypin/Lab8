40e06c0e599e98d29625d2d03099d5499685dd2f
IvanStolypin <i.stolypin.d@gmail.com>
2024-10-25 14:21:07 -0600

    deleteCity fail test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -43,5 +43,17 @@ public class CustomListTest {
         assertEquals(true, list.hasCity(city)); // This will initially fail
     }
 
+    @Test
+    public void deleteCityTest() {
+        list = MockCityList();
+        City city = new City("Regina", "SK");
+        list.addCity(city); // Add city first
+        assertEquals(true, list.hasCity(city)); // Ensure city was added
+
+        list.deleteCity(city); // Delete city
+        assertEquals(false, list.hasCity(city)); // Check if city was deleted
+    }
+
+
 
 }
